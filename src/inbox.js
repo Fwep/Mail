e4ff6c903f000d5dc6c6bf638a9d1389a88dc8d9
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 21:29:31 -0700

    Refactor render to pull messages from MessageStore, and delegate rendering of each message to helper

--- src/inbox.js
+++ src/inbox.js
@@ -1,9 +1,27 @@
+const MessageStore = require('./message_store');
+
 const Inbox = {
   render: function() {
     let containerUl = document.createElement('ul');
     containerUl.className = 'messages';
-    containerUl.innerHTML = 'An Inbox Message';
+    let inboxMsgs = MessageStore.getInboxMessages();
+    inboxMsgs.forEach(msg => {
+      console.log(this);
+      let messageNode = this.renderMessage(msg);
+      containerUl.appendChild(messageNode);
+    });
+    
     return containerUl;
+  },
+  renderMessage: (message) => {
+    let li = document.createElement('li');
+    li.className = 'message';
+    li.innerHTML = `
+    <span class="from">${message.from}</span>
+    <span class="subject">${message.subject}</span>
+    <span class="body">${message.body}</span>
+    `
+    return li;
   }
 }
 

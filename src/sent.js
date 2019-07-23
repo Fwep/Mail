78196845761337a5e6a3065a96c825f4ae8beeba
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 22:10:23 -0700

    Create Sent component

---
+++ src/sent.js
@@ -0,0 +1,28 @@
+const MessageStore = require('./message_store');
+
+const Sent = {
+  render: function() {
+    let containerUl = document.createElement('ul');
+    containerUl.className = 'messages';
+    let inboxMsgs = MessageStore.getSentMessages();
+    inboxMsgs.forEach(msg => {
+      console.log(this);
+      let messageNode = this.renderMessage(msg);
+      containerUl.appendChild(messageNode);
+    });
+
+    return containerUl;
+  },
+  renderMessage: (message) => {
+    let li = document.createElement('li');
+    li.className = 'message';
+    li.innerHTML = `
+    <span class="to">${message.to}</span>
+    <span class="subject">${message.subject}</span>
+    <span class="body">${message.body}</span>
+    `
+    return li;
+  }
+}
+
+module.exports = Sent;

43f1b324213748bb7544aaa35dd6402dd115cf23
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 21:13:59 -0700

    Create MessageStore component
    Create component that has function properties that
    retrieve sent and received messages from a private messages
    object.

---
+++ src/message_store.js
@@ -0,0 +1,24 @@
+const messages = {
+  inbox: [
+    {from: 'mary@peacetable.com', subject: 'Re: Onsite Interview', body: 'Hey bud\n
+    we ended up \"pursuing other candidates\" because you were too good for us...sorry!'},
+    {from: 'mom@mother.mommy', subject: 'Love you Nong', body: 'Hi nong, study hard ok...'}
+  ],
+  sent: [
+    {to: 'recruiter@google.com', subject: 'Come hire me!', body: 'I\'m an awesome developer!'},
+    {to: 'elon@spacex.tes', subject: 'About that electric VTOL prototype...', 
+    body: 'Hey dude, I was able to run sims on SolidWorks and the deflection of the propeller seemed within\n
+    tolerance'}
+  ]
+}
+
+const MessageStore = {
+  getInboxMessages: function() {
+    return messages.inbox;
+  },
+  getSentMessages: function() {
+    return messages.sent;
+  }
+}
+
+module.exports = MessageStore;

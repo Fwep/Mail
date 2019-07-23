8929adbab1d9a41ada60c79629018676112d0bfb
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 21:30:19 -0700

    Define routes, and route to #inbox upon load

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+const Router = require('./router');
+const Inbox = require('./inbox');
+
+const routes = {
+  compose: null,
+  inbox: Inbox,
+  sent: null
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  let contentNode = document.querySelector('.content')
+  let router = new Router(contentNode, routes);
+  router.start();
+  window.location.hash = '#inbox';
+  
   let sidebarItems = document.querySelectorAll('.sidebar-nav li');
   sidebarItems.forEach(item => {
     item.addEventListener('click', () => {
-      let {innerText} = item;
+      let innerText = item.innerText;
       window.location.hash = innerText.toLowerCase();
     })
   });
 });
+
+window.Router = Router;

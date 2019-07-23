7ab57a4cb9c4ea0f7c6312a30c91a9acbdee02b3
Taran Cacacho <tarancacacho@gmail.com>
2019-07-22 19:05:39 -0700

    Add event listener to change hash fragment of URL upon click events on sidebar items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("It's working")
+document.addEventListener('DOMContentLoaded', () => {
+  let sidebarItems = document.querySelectorAll('.sidebar-nav li');
+  sidebarItems.forEach(item => {
+    item.addEventListener('click', () => {
+      let {innerText} = item;
+      window.location.hash = innerText.toLowerCase();
+    })
+  });
+});
